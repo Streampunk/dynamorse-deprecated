10afc57d558b57d7ab8b5990d5ba296437d613b9
Richard Cartwright <richard@portability4media.com>
2015-12-27 16:22:25 +0000

    Working on SDP parser.

--- pipeline/rawToGrain.js
+++ pipeline/rawToGrain.js
@@ -12,17 +12,3 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-
-var Grain = require('../model/Grain.js');
-var Timecode = require('../model/Timecode.js');
-var test = require('tape');
-
-test('A newly created grain from mainly strings', function (t) {
-  var g = new Grain([new Buffer(10), new Buffer(5)],
-    '12345:67890', '98765:43210', '10:11:12:13',
-    'CCA06626-AEB1-4014-8287-BBEF45E7DD51',
-    '0313B546-1325-4983-B80B-1C3C312FC181',
-    '25/1');
-  console.log(JSON.stringify(g, null, 2));
-  t.end();
-});
