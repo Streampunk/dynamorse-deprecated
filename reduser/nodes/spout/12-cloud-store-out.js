2b15208f31caa4fb4b9ba10a39dc705f37ef7e61
Streampunk Media <furnace@streampunk.media>
2016-04-01 17:12:49 +0100

    Adding renamed nodes.

---
+++ reduser/nodes/spout/12-cloud-store-out.js
@@ -0,0 +1,26 @@
+/* Copyright 2016 Christine S. MacNeill
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by appli cable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+var redioactive = require('../../../util/Redioactive.js');
+
+module.exports = function (RED) {
+  function CloudStoreOut (config) {
+    RED.nodes.createNode(this, config);
+    redioactive.Spout.call(this, config);
+    // Go figure!
+  }
+  util.inherits(CloudStoreOut, redioactive.Spout);
+  RED.nodes.registerType("cloud-store-out", CloudStoreOut);
+}
