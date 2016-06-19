7a6c0cd75f8bf4acf009d9e90667f19eb20935c0
Streampunk Media <furnace@streampunk.media>
2016-06-19 16:32:00 +0100

    Bringing HTTP experiments back to life.

--- reduser/nodes/funnel/40-spm-http-in.js
+++ reduser/nodes/funnel/40-spm-http-in.js
@@ -26,49 +26,50 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     redioactive.Funnel.call(this, config);
 
-    // var protocol = (config.protocol === 'HTTP') ? http : https;
-    //
-    // var runNext = function (x) {
-    //   protocol.get(
-    //     `${config.pullUrl}${(config.pullUrl.endsWith('/') ? '' : '/')}${config.path}`,
-    //     function (res) {
-    //       var count = 0;
-    //       res.on('data', function (data) {
-    //         count++; // Concatenate to buffer
-    //       });
-    //       res.on('end', function () {
-    //         total++;
-    //         tally += process.hrtime(startTime)[1]/1000000;
-    //         console.log("No more data!", tally / total, total, x, count);
-    //         runNext.call(this, x);
-    //       });
-    //     }).on('error', (e) => {
-    //       this.warn(`Received error when requesting frame from server on thread ${x}: ${e}`)
-    //     });
-    //   }
-    // };
-    //
-    // if (config.mode === 'push') {
-    //   var app = express();
-    //   app.use(bodyParser.raw({ limit : config.payloadLimit || 6000000 }));
-    //
-    //   app.post(config.path, function (req, res) {
-    //     console.log(pushCount++, process.hrtime(startTime), req.body.length);
-    //     res.json({ length : req.body.length }); // Need to add back pressure concept
-    //   });
-    //
-    //   var server = protocol.createServer((config.protocol === 'HTTP') ? {} : {
-    //     key : fs.readFileSync('./certs/dynamorse-key.pem'),
-    //     cert : fs.readFileSync('./certs/dynamorse-cert.pem')
-    //   }, app).listen(config.port);
-    //   server.on('listening', function () {
-    //     this.log(`Dynamorse ${config.protocol} server listening on port ${config.port}.`);
-    //   });
-    // } else { // config.mode is set to pull
-    //   for ( var x = 0 ; x < config.threads ; x++ ) { // Number of parallel requests
-    //     runNext.call(this, x);
-    //   }
-    // }
+    var protocol = (config.protocol === 'HTTP') ? http : https;
+
+    var runNext = function (x) {
+      protocol.get(
+        `${config.pullUrl}${(config.pullUrl.endsWith('/') ? '' : '/')}${config.path}`,
+        function (res) {
+          var count = 0;
+          res.on('data', function (data) {
+            count++; // Concatenate to buffer
+          });
+          res.on('end', function () {
+            total++;
+            tally += process.hrtime(startTime)[1]/1000000;
+            console.log("No more data!", tally / total, total, x, count);
+            runNext.call(this, x);
+          });
+        }).on('error', (e) => {
+          this.warn(`Received error when requesting frame from server on thread ${x}: ${e}`)
+        });
+      }
+    };
+
+    if (config.mode === 'push') {
+      var app = express();
+      app.use(bodyParser.raw({ limit : config.payloadLimit || 6000000 }));
+    
+      app.post(config.path, function (req, res) {
+        console.log(pushCount++, process.hrtime(star
+          tTime), req.body.length);
+        res.json({ length : req.body.length }); // Need to add back pressure concept
+      });
+
+      var server = protocol.createServer((config.protocol === 'HTTP') ? {} : {
+        key : fs.readFileSync('./certs/dynamorse-key.pem'),
+        cert : fs.readFileSync('./certs/dynamorse-cert.pem')
+      }, app).listen(config.port);
+      server.on('listening', function () {
+        this.log(`Dynamorse ${config.protocol} server listening on port ${config.port}.`);
+      });
+    } else { // config.mode is set to pull
+      for ( var x = 0 ; x < config.threads ; x++ ) { // Number of parallel requests
+        runNext.call(this, x);
+      }
+    }
   }
   util.inherits(SpmHTTPIn, redioactive.Funnel);
   RED.nodes.registerType("spm-http-in", SpmHTTPIn);
